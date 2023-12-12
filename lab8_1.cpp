44afb8985b88bcabd7da9be9b88ba9f54f188aff
Korn Sriwattana <korn_s@cmu.ac.th>
2023-12-12 15:36:04 +0700

    editing lab8.1 เสร็จถูกต้องล้าว

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -9,15 +9,14 @@ int main()
   cin >> rank;
   if( rank == 'S')
     cout << "You have received Super Ultra Rare Unit!!!\n";
-  else if( rank == 'A')
+  if( rank == 'S'|| rank =='A')
     cout << "You have received 5 gems.\n";
-  else if( rank == 'B')
+  if( rank == 'S' || rank =='A' || rank =='B')
     cout << "You have received 1 gems.\n";
-  else if( rank == 'C')
+  if( rank == 'S' || rank =='A' || rank =='B' || rank =='C')
     cout << "You have received 2000 coins.\n";
-  else if( rank == 'D')
+  if( rank == 'S' || rank =='A' || rank =='B' || rank =='C' || rank =='D') 
     cout << "You have received very KAK items.\n";
 
   return 0;
 }
-
